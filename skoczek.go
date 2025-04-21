0af7f3cdf44fc803b5c4ce85061b54994eceb930
Piotr Łopatecki <piotr.lopatecki@gmail.com>
2025-04-21 19:10:04 +0200

    rozwiązanie algorytmem Warnsdorff-a oraz dodanie readme

--- skoczek.go
+++ skoczek.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,8 +10,7 @@ var px int
 var py int
 var plansza [][]int
 var n int
-var tura int
-var odwiedzone_pola int
+var NextXmove, NextYMove int
 
 func main() {
 
@@ -47,28 +47,68 @@ func main() {
 		fmt.Println("Nie podałeś liczby!")
 		return
 	}
-	odwiedzone_pola = 0
-	tura = 1
+	tura := 1
 	fmt.Println("Plansza:")
-	for _, row := range solve(n, plansza, odwiedzone_pola, [2]int{px, py}, tura) {
+	answer := Warnsdorff(n, plansza, [2]int{px, py}, tura)
+	for _, row := range answer {
 		fmt.Println(row)
 	}
 }
 
-func solve(n int, plansza [][]int, odwiedzone_pola int, position [2]int, tura int) [][]int {
-	if odwiedzone_pola == n*n {
-		return plansza
-	}
-	plansza[position[1]-1][position[0]-1] = tura
+func count_moves(plansza [][]int, position [2]int, n int) int {
+	// tablice dostępnych ruchów
 	Ymoveset := [8]int{-1, 1, 2, 2, -2, -2, -1, 1}
 	Xmoveset := [8]int{-2, -2, -1, 1, -1, 1, 2, 2}
+	counter := 0
 	for i := 1; i <= 8; i++ {
 		NextXmove := position[0] + Xmoveset[i-1]
 		NextYmove := position[1] + Ymoveset[i-1]
+		// Jeżeli można wykonać ruch, podlicz go
 		if NextXmove <= n && NextXmove >= 1 && NextYmove <= n && NextYmove >= 1 && plansza[NextYmove-1][NextXmove-1] == 0 {
-			tura++
-			solve(n, plansza, odwiedzone_pola+1, [2]int{NextXmove, NextYmove}, tura)
+			counter++
 		}
 	}
+	return counter
+}
+
+func Warnsdorff(n int, plansza [][]int, position [2]int, tura int) [][]int {
+	// Jeżeli liczba wykonanych ruchów równa się liczbie wszystkich pól planszy zwróć rozwiązaną planszę
+	if tura == n*n {
+		plansza[position[1]-1][position[0]-1] = tura
+		return plansza
+	}
+	// Największa liczba 64 bitowa
+	min_counter := math.MaxInt
+	// tablice dostępnych ruchów
+	Ymoveset := [8]int{-1, 1, 2, 2, -2, -2, -1, 1}
+	Xmoveset := [8]int{-2, -2, -1, 1, -1, 1, 2, 2}
+	// Zaznaczenie w komórce na której skoczek się znajduję, który to jest ruch
+	plansza[position[1]-1][position[0]-1] = tura
+	for i := 1; i <= 8; i++ {
+		// tymczasowe wartości x i y
+		temp_x := position[0] + Xmoveset[i-1]
+		temp_y := position[1] + Ymoveset[i-1]
+		// liczba możliwych ruchów dla tymczasowych pozycji
+		count := count_moves(plansza, [2]int{temp_x, temp_y}, n)
+		// Sprawdzenie czy ruch można wykonać
+		if temp_x <= n && temp_x >= 1 && temp_y <= n && temp_y >= 1 && plansza[temp_y-1][temp_x-1] == 0 {
+			// Jeżeli liczba możliwych ruchów jest większa od najnowszej minimalnej liczby możliwych ruchów nadpisz pozycję i NOWE tymczasowe pozycję
+			if count < min_counter {
+				min_counter = count
+				// tymczasowe NOWE pozycje skoczka
+				NextXmove = temp_x
+				NextYMove = temp_y
+			}
+		} else {
+			continue
+		}
+
+	}
+	// Zmiana pozycji skoczka
+	position[0] = NextXmove
+	position[1] = NextYMove
+	// Inkrementacja tury
+	tura++
+	Warnsdorff(n, plansza, position, tura)
 	return plansza
 }
